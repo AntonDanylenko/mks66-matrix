462a4fbe2ec89e00e72452042eb0cdb9d26305f5
Anton Danylenko <adanylenko@stuy.edu>
2019-02-16 20:50:41 -0500

    s

--- draw.py
+++ draw.py
@@ -10,8 +10,8 @@ def draw_lines( matrix, screen, color ):
 
 
 def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
-    matrix.append([x0,y0,z0,1])
-    matrix.append([x1,y1,z1,1])
+    add_point(matrix,x0,y0,z=z0)
+    add_point(matrix,x1,y1,z=z1)
 
 def add_point( matrix, x, y, z=0 ):
     matrix.append([x,y,z,1])
--- main.py
+++ main.py
@@ -7,24 +7,26 @@ color = [ 0, 255, 0 ]
 matrix = new_matrix()
 
 add_edge(matrix,150,250,0,350,250,0)
-add_edge(matrix,350,250,0,300,400,0)
-add_edge(matrix,300,400,0,200,400,0)
-add_edge(matrix,200,400,0,150,250,0)
+add_edge(matrix,350,250,0,300,350,0)
+add_edge(matrix,300,350,0,200,350,0)
+add_edge(matrix,200,350,0,150,250,0)
 add_edge(matrix,150,250,0,200,100,0)
-add_edge(matrix,200,100,0,250,075,0)
-add_edge(matrix,250,075,0,350,150,0)
+add_edge(matrix,200,100,0,250,100,0)
+add_edge(matrix,250,100,0,350,150,0)
 print_matrix(matrix)
 
-#ident(matrix)
-#print_matrix(matrix)
-
-transformation = new_matrix()
-transformation[0] = [1,0,0,0]
-transformation[1] = [0,1,0,0]
-transformation[2] = [0,0,1,0]
-transformation[3] = [0,0,0,1]
-#matrix_mult(transformation, matrix)
-#print_matrix(matrix)
+A = new_matrix()
+B = new_matrix()
+A = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}}
+B = {{11,12,13,14},{15,16,17,18},{19,20,21,22},{23,24,25,26}}
+matrix_mult(A,B)
+print_matrix(A)
+print_matrix(B)
+matrix_mult(B,A)
+print_matrix(A)
+print_matrix(B)
+matrix_mult(ident(B),A)
+print_matrix(A)
 
 draw_lines( matrix, screen, color )
 display(screen)
