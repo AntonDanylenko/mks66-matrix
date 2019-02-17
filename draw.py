fd3517141e29e751bcbd1769a41387c58d95aadc
Anton Danylenko <adanylenko@stuy.edu>
2019-02-16 21:44:01 -0500

    s

--- draw.py
+++ draw.py
@@ -21,7 +21,7 @@ def add_multi(matrix, coords):
         coords_list[i] = coords_list[i].split(",")
         for ii in range(len(coords_list[i])):
             coords_list[i][ii] = int(int(coords_list[i][ii])*900.0/500)
-            print(coords_list[i][ii] + " ")
+            print(str(coords_list[i][ii]) + " ")
         print("\n")
     add_edge(matrix,coords_list[0][0],0,0,coords_list[0][0],coords_list[0][1],0)
     for i in range(len(coords_list)-1):
